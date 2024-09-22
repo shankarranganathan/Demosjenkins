110211d604caaec47138cb0531c83b6f21b31df8
shankarranganathan <shankarmech100@gmail.com>
2024-09-22 06:20:51 +0530

    shancommit

---
+++ src/org/demos/Demos.java
@@ -0,0 +1,13 @@
+package org.demos;
+
+public class Demos {
+	public static void main(String[] args) {
+		System.out.println("shankar");
+		System.out.println("hansika");
+		System.out.println("jai");
+		System.out.println("siva");
+		System.out.println("Ranganathan");
+		System.out.println("sundari");
+	}
+
+}
